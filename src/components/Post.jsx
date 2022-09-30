713516e33799b6afcd9a9045e85810e49db63739
Eduardo Winter <eduardowb@gmail.com>
2022-09-30 10:32:04 -0300

    Added Post compononent

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/eduardowinter.png" />
+          <div className={styles.authorInfo}>
+            <strong>Eduardo Winter</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="30 de Setembro às 08:55h" dateTime="2022-09-30 08:55:25">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
